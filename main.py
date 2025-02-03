323a4f36705072e4389731d14f572a4c06d6419e
AstoratHONK <wh40world@icloud.com>
2025-02-03 06:24:00 +0100

    add_fastAPI3

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 import subprocess
+import uvicorn
+import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 app = FastAPI()
 
 @app.get("/")
